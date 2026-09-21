26bdc96c84c03654e15373d1a78884b10d44f233
amorafut <amorafut@gmail.com>
2026-09-20 23:01:29 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-window.AMORA_CONFIG={storeName:'Amora Fut Streetwear',domain:'amorafut.com.br',instagram:'https://instagram.com/amorafutcamisas',whatsappNumber:'',pixDiscount:0.05,currency:'BRL'};
+window.AMORA_CONFIG={storeName:'Amora Fut Streetwear',domain:'amorafut.com.br',instagram:'https://instagram.com/amorafutcamisas',whatsappNumber:'5585998022643',pixDiscount:0.05,currency:'BRL'};
